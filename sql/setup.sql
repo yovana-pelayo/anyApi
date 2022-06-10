1b7859f0534e7fa2bdd592bcbe9c80325bff9d8d
Yovana Pelayo <yovana_pelayo@yahoo.com>
2022-06-10 15:23:04 -0700

    syntax error in setup.sql. api data is displaying in beekeeper local host

--- sql/setup.sql
+++ sql/setup.sql
@@ -1,15 +1,15 @@
 -- Use this file to define your SQL tables
 -- The SQL in this file will be executed when you run `npm run setup-db`
-DROP table if exists Coffee;
+DROP table if exists coffee;
 
 CREATE table coffee(
     id BIGINT GENERATED ALWAYS AS IDENTITY,
     name VARCHAR NOT NULL,
     type VARCHAR NOT NULL,
-    price INT NOT NULL,
+    money INT NOT NULL
 );
 
-INSERT INTO coffee(name, type, price) VALUES ('Mocha', 'iced', 5
+INSERT INTO coffee (name, type, money) VALUES ('Mocha', 'iced', 5
 ),
 ('latte', 'hot', 4.5), 
 ('frapp', 'blended', 5.5);
